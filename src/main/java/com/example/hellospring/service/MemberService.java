228ea6322d07104b5163d26bf006f78615af9ccf
Jaekeun Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-12-18 23:56:59 +0900

    Initial commit

---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+
+        //같은 이름이 있는 중복회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+}
+
